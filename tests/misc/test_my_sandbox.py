b49b45c541d60e0bb5119a5aa006ae0bae6527a2
Frédéric Zind <frederic.zind@vates.tech>
2026-09-08 12:07:12 +0200

    Add a VM test using SSH
    SSH authentication is not managed by test suite
    
    Signed-off-by: Frédéric Zind <frederic.zind@vates.tech>

--- tests/misc/test_my_sandbox.py
+++ tests/misc/test_my_sandbox.py
@@ -5,15 +5,13 @@ from lib.vm import VM
 
 # A playground to build KISS tests
 #
+#    Requirement:
+#    - a host (--hosts=<IP>)
+#    - a VM   (--vm=<UUID> or it uses PXE)
+
 
 
 def test_vm_stop_start(imported_vm: VM) -> None:
-    """
-    Requirement:
-    - a host (--hosts=<IP>)
-    - a VM   (--vm=<UUID> or it uses PXE)
-    """
-
     vm = imported_vm
     if (vm.is_running()):
         logging.info("VM already running, shutting it DOWN first ⌛")
@@ -28,3 +26,19 @@ def test_vm_stop_start(imported_vm: VM) -> None:
     vm.start()
     vm.wait_for_os_booted()
     logging.info("VM is UP again ✅")
+
+
+def test_file_create_delete_on_vm(imported_vm: VM) -> None:
+    vm = imported_vm
+    tmp_file = f"/tmp/file_from-{test_file_create_delete_on_vm.__name__}"
+
+    if (not vm.is_running):
+        logging.info("VM is DOWN, starting it now ⌛")
+        vm.start()
+
+    vm.try_get_and_store_ip()
+    vm.ssh_touch_file(tmp_file)
+    assert vm.file_exists(tmp_file)
+
+    vm.ssh(f"rm -f {tmp_file}")
+    assert not vm.file_exists(tmp_file)
